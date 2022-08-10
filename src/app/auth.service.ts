42cce10404df91e3095df4d23f14dd8ed99d1a6a
pawelzdrojewski <zdrojewski_paw@wp.pl>
2022-08-10 12:44:03 +0200

    10.08.2022 P

---
+++ src/app/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  login(){
+    return true;
+
+  }
+  logout(){
+    return false;
+
+  }
+}

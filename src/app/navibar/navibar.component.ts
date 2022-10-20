b4b8ba95bfa84f1275b30ee4fa957405703a94c9
dom <zdrojewski_paw>
2022-10-21 00:10:30 +0200

    dynamiczne wypenianie menu zalezne od roli

--- src/app/navibar/navibar.component.ts
+++ src/app/navibar/navibar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
 import { HttpService } from '../http.service';
 
@@ -12,22 +14,22 @@ export class NavibarComponent implements OnInit {
 
   //load_badge = this.http.loader_badge;
   badge?: number;
-<<<<<<< HEAD
-  role_admin = this.Auth.role_admin.value;
-=======
-  role_admin: boolean = false;
->>>>>>> b268930ecec1e8a2acccf782624d65d34ec91721
 
-  navlink: {navi: string, link: string, role: string}[] = [
-    {navi: "Faktury", link: "/faktury", role: "user"},
+  role_admin = this.Auth.role_admin.value;
+
+  navlinks: {navi: string, link: string, role: string}[] = [
+    {"navi": "Faktury", link: "/faktury", role: "user"},
     {navi: "Liczniki", link: "/liczniki", role: "user"},
     {navi: "Odczyt licznika", link: "/odczyty", role: "user"},
     {navi: "Zgłoszenia", link: "/zgloszenie", role: "user"},
-    {navi: "Statystyki", link: "/faktury", role: "user"},
-    {navi: "Umowy", link: "/faktury", role: "user"},
-    {navi: "Pomoc", link: "/faktury", role: "user"},
+    {navi: "Statystyki", link: "#", role: "user"},
+    {navi: "Umowy", link: "#", role: "user"},
+    {navi: "Pomoc", link: "#", role: "user"},
     {navi: "Komunikaty", link: "/komunikaty", role: "admin"},
   ];
+
+  filter_navilinks?: {navi: string, link: string, role: string}[];
+  
   constructor( public Auth: AuthService,
                private router: Router,
                private http: HttpService) { }
@@ -36,8 +38,8 @@ export class NavibarComponent implements OnInit {
       this.http.notifi_subject.subscribe(
     //this.http.loader_badge.subscribe(   //????? taki i tak działa 
       (badge) => {this.badge = badge}
-    );
-    console.log(this.navlink[0].link)
+    ); 
+    this.filter_navilinks =  this.navlinks.filter(navilinks => navilinks.role == "admin" || "user");
   }
   out(){
     this.Auth.logout();
